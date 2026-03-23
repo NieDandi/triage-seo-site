bf26408099b0938fcd953e7b59fe7f8c57e25db1
NieDandi <3539098036@qq.com>
2026-03-23 14:18:01 +0800

    Add files via upload

--- app/robots.ts
+++ app/robots.ts
@@ -5,11 +5,6 @@ const baseUrl = "https://triage-seo-site.vercel.app";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: [
-      `${baseUrl}/sitemap/0.xml`,
-      `${baseUrl}/sitemap/1.xml`,
-      `${baseUrl}/sitemap/2.xml`,
-      `${baseUrl}/sitemap/3.xml`,
-    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
